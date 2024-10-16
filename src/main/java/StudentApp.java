e2080518da81d415fffba5519b2fd8a58dcbbfc2
davidprendergast <g00405007@atu.ie>
2024-10-16 22:17:49 +0100

    Student App

--- src/main/java/StudentApp.java
+++ src/main/java/StudentApp.java
@@ -1,2 +1,25 @@
+package atu.ie;
+
+import java.util.Scanner;
+
+
 public class StudentApp {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = input.nextLine();
+        System.out.println("Enter your email:");
+        String email = input.nextLine();
+        System.out.println("Enter your course:");
+        String course = input.nextLine();
+        System.out.println("");
+        System.out.println("");
+
+        //first person
+        Student student = new Student();
+        student.setName(name);
+        student.setName(email);
+        student.setName(course);
+    }
 }
